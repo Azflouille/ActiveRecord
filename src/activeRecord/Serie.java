da05bd57ab6658808e6be87f4525aa5a1ccb1c3e
Azflouille <90977486+Azflouille@users.noreply.github.com>
2021-12-13 17:15:45 +0100

    commit droptable

--- src/activeRecord/Serie.java
+++ src/activeRecord/Serie.java
@@ -49,6 +49,56 @@ public class Serie {
         return serie;
     }
 
+    public ArrayList<Serie> findByName(String search) throws SQLException {
+        ArrayList series = new ArrayList<Serie>();
+        Connection connect = DBConnection.getInstance().getConnection();
+        String query = ("SELECT * FROM SERIE WHERE NOM LIKE ?");
+        PreparedStatement stmt = connect.prepareStatement(query);
+        stmt.setString(1, "%" + search + "%");
+        ResultSet resultset = stmt.executeQuery();
+        while (resultset.next()) {
+            Serie serie = new Serie(resultset.getString("nom"), resultset.getString("genre"));
+            int ID = resultset.getInt("ID");
+            serie.setID(ID);
+            series.add(serie);
+        }
+        return series;
+    }
 
+    public ArrayList<Serie> findByGenre(String genre) throws SQLException {
+        ArrayList series = new ArrayList<Serie>();
+        Connection connect = DBConnection.getInstance().getConnection();
+        String query = ("SELECT * FROM SERIES WHERE GENRE = ?");
+        PreparedStatement stmt = connect.prepareStatement(query);
+        stmt.setString(1, genre);
+        ResultSet resultset = stmt.executeQuery();
+        while (resultset.next()) {
+            Serie serie = new Serie(resultset.getString("nom"), resultset.getString("genre"));
+            int ID = resultset.getInt("ID");
+            serie.setID(ID);
+            series.add(serie);
+        }
+        return series;
+    }
+
+    public static void createTable() throws SQLException {
+        Connection connect = DBConnection.getInstance().getConnection();
+        String query = ("CREATE TABLE `serie` (\n" +
+                "  `ID` int(11) NOT NULL,\n" +
+                "  `NOM` varchar(40) NOT NULL,\n" +
+                "  `GENRE` varchar(40) NOT NULL\n" +
+                ") ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+        Statement stmt = connect.createStatement();
+        stmt.executeUpdate(query);
+    }
+
+    public static void deleteTable() throws SQLException {
+        Connection connect = DBConnection.getInstance().getConnection();
+        String query = ("DROP TABLE SERIE");
+        Statement stmt = connect.createStatement();
+        stmt.executeUpdate(query);
+    }
+
+    
 
 }
